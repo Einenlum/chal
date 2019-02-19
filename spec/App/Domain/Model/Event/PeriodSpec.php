c2766e05f2064c881633919a0e34ba2f228570d7
Einenlum <yann.rabiller@gmail.com>
2019-02-19 16:35:52 +0100

    Spec Period

---
+++ spec/App/Domain/Model/Event/PeriodSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\App\Domain\Model\Event;
+
+use App\Domain\Model\Event\Period;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use App\Domain\Exception\InvalidPeriodException;
+
+class PeriodSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->beConstructedWith(
+            new \DateTimeImmutable(),
+            new \DateTimeImmutable()
+        );
+        $this->shouldHaveType(Period::class);
+    }
+
+    function it_is_constructed_with_valid_dates()
+    {
+        $start = new \DateTimeImmutable();
+        $end = new \DateTimeImmutable('+1 day');
+
+        $this->beConstructedWith($start, $end);
+        $this->getStart()->shouldReturn($start);
+        $this->getEnd()->shouldReturn($end);
+    }
+
+    function it_throws_exception_if_constructed_with_invalid_dates()
+    {
+        $start = new \DateTimeImmutable('+1 day');
+        $end = new \DateTimeImmutable('-1 day');
+
+        $this
+            ->shouldThrow(InvalidPeriodException::class)
+            ->during('__construct', [$start, $end])
+        ;
+    }
+}
