c32ca37a22efadbd347cb8143eb7f1c5d49a6c69
Einenlum <yann.rabiller@gmail.com>
2019-02-20 14:21:49 +0100

    Update description of the migration

--- src/Infrastructure/Doctrine/Migrations/Version20190220074229.php
+++ src/Infrastructure/Doctrine/Migrations/Version20190220074229.php
@@ -7,19 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190220074229 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add position for places';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE event CHANGE place_id place_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\'');
@@ -29,7 +25,6 @@ final class Version20190220074229 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE event CHANGE place_id place_id CHAR(36) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:uuid)\'');
